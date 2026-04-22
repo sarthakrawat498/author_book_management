85437acbf3a65831babdfb0fad5e524cb6990fa1
Sarthak Rawat <sarthak.rawat@nagarro.com>
2026-04-22 16:40:20 +0530

    jenkins and docker

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM eclipse-temurin:21-jdk
+
+WORKDIR /App
+
+COPY target/author_book_management-0.0.1-SNAPSHOT.jar app.jar
+
+# Container's port
+EXPOSE 8082
+
+# What to run
+CMD ["java", "-jar", "app.jar"]
--- src/test/java/com/sarthak/library/author_book_management/controller/BookControllerTest.java
+++ src/test/java/com/sarthak/library/author_book_management/controller/BookControllerTest.java
@@ -19,6 +19,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 import java.util.List;
 
